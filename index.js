898f5cbff1c355a8ee58b466bdb005abac9bfe17
Alexander Zaytsev <alexander@say26.com>
2015-07-09 11:03:49 +0700

    Convert en_US to en-US for i18n.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 var I18n = require('./vendor/i18n');
 var { RNI18n } = require('react-native').NativeModules;
 
-I18n.locale = RNI18n.locale;
+I18n.locale = RNI18n.locale.replace(/_/, '-');
 
 module.exports = I18n;
