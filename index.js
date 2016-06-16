c6691e049a4005aea6d082e65692d60fa1aa22ef
Samuli Hakoniemi <samuli@hakoniemi.net>
2016-06-16 15:39:55 +0300

    Uniform code and fix for test friendliness (mocha)
    - replace let with const
    - check existence of `RNI18n` when accessing it's properties
    - remove unnecessary code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-let { Platform, NativeModules } = require('react-native')
-let { RNI18n } = NativeModules
-let I18n = require('./vendor/i18n')
+const { NativeModules } = require('react-native');
+const { RNI18n } = NativeModules;
+let I18n = require('./vendor/i18n');
 
-I18n.locale = RNI18n.locale.replace(/_/, '-');
+I18n.locale = (RNI18n) ? RNI18n.locale.replace(/_/, '-') : '';
 
 module.exports = I18n;
