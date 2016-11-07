d207c503a7abdbf0c8faec4a5a9c6d78cc455ccf
Joseph An <an.joseph891@gmail.com>
2016-11-07 14:06:56 -0800

    Template files

---
+++ windows/RNI18n/Class1.cs
@@ -0,0 +1,18 @@
+﻿using ReactNative.Bridge;
+using ReactNative.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace ReactNative.Modules.RNI18n
+{
+    public class ReactNativeI18n : NativeModuleBase
+    {
+        public override string Name
+        {
+            get
+            {
+                return "RNI18n";
+            }
+        }
+    }
+}
