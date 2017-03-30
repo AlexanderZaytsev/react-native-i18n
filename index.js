a19790da3935dda3ff3e03ed3e3642b0cd2dd30a
Mathieu Acthernoene <zoontek@gmail.com>
2017-03-30 02:06:17 +0200

    Define I18n.js as a dependency

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const { NativeModules } = require('react-native');
 const { RNI18n } = NativeModules;
-let I18n = require('./vendor/i18n');
+let I18n = require('i18n-js');
 
 I18n.locale = (RNI18n) ? RNI18n.locale.replace(/_/, '-') : '';
 
