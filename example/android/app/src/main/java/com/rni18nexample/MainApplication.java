d5ad3b3d0d08dc45b37f36b0e93f8854b9755c60
Mathieu Acthernoene <zoontek@gmail.com>
2017-04-06 02:02:10 +0200

    react-native link on example

--- example/android/app/src/main/java/com/rni18nexample/MainApplication.java
+++ example/android/app/src/main/java/com/rni18nexample/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.alexanderzaytsev.rn18n.RNI18nPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +23,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNI18nPackage()
       );
     }
   };
