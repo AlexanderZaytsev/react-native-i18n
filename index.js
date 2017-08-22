5f094a94128ce7e0986ef67c1e859d57871cef4d
Fábio Pereira <1fabiopereira@users.noreply.github.com>
2017-08-22 12:48:11 -0300

    Friendly test fix

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 
 import { NativeModules } from 'react-native';
 import I18n from 'i18n-js';
+
 const { RNI18n } = NativeModules;
 
-I18n.locale = RNI18n.languages[0];
+I18n.locale = (RNI18n) ? RNI18n.languages[0] : "";
 
-export const getLanguages = RNI18n.getLanguages;
+export const getLanguages = (RNI18n) ? RNI18n.getLanguages : () => {};
+
 export default I18n;
