d181ecb697f23254a9516c0749b4506df3c73aa8
Mathieu Acthernoene <zoontek@gmail.com>
2017-08-27 20:24:10 +0200

    Add test & mock example

---
+++ example/src/__mocks__/react-native-i18n.js
@@ -0,0 +1,7 @@
+// @flow
+
+import I18nJs from 'i18n-js';
+
+I18nJs.locale = 'en'; // a locale from your available translations
+export const getLanguages = () => Promise.resolve('en');
+export default I18nJs;
