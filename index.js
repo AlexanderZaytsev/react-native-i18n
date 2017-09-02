91d30e1d3205f039de1c1ea8d9a4dedea074eee5
Vlad Romanovich <romanovich.vo@gmail.com>
2017-09-02 13:08:31 +0300

    Added default value for locale and getLanguages

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 const RNI18n = require('react-native').NativeModules.RNI18n;
 const I18nJs = require('i18n-js');
 
-I18nJs.locale = RNI18n.languages[0];
-export const getLanguages = RNI18n.getLanguages;
+I18nJs.locale = RNI18n ? RNI18n.languages[0] : 'en';
+export const getLanguages = RNI18n ? RNI18n.getLanguages : new Promise((resolve, reject) => {
+    resolve('en');
+});
+
 export default I18nJs;
