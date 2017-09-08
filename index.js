13c8bd50c71ee98b6dfdea73a974caa425870b80
Mathieu Acthernoene <zoontek@gmail.com>
2017-09-08 21:39:23 +0200

    Add a warning when the package is not linked

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
 const RNI18n = require('react-native').NativeModules.RNI18n;
 const I18nJs = require('i18n-js');
 
-I18nJs.locale = RNI18n.languages[0];
+if (typeof RNI18n !== 'undefined') {
+  I18nJs.locale = RNI18n.languages[0];
+} else if (__DEV__) {
+  console.warn('react-native-i18n module is not correctly linked');
+}
+
 export const getLanguages = RNI18n.getLanguages;
 export default I18nJs;
