337b3a20be71aac6c6509703877c45d76f0012b7
Sofiane Soufi <sofiane.soufi@gmail.com>
2017-11-18 03:53:51 -0500

    Fix undefined default value for getLanguages now fixed by setting one.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const RNI18n = require('react-native').NativeModules.RNI18n;
 const I18nJs = require('i18n-js');
 
 if (typeof RNI18n !== 'undefined') {
-  I18nJs.locale = RNI18n.languages[0];
+  I18nJs.locale = (RNI18n) ? RNI18n.languages[0] : "";
 } else if (__DEV__) {
   console.warn('react-native-i18n module is not correctly linked');
 }
 
-export const getLanguages = RNI18n.getLanguages;
+export const getLanguages = (RNI18n) ? RNI18n.getLanguages : () => {};
 export default I18nJs;
