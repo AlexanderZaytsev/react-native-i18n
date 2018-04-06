0f7572cf6dcbce8ac02ec5c52bed0244c24afa4d
Chafik <chafik.hnini@gmail.com>
2018-04-06 14:58:34 +0200

    Add missing flow annotations

--- example/src/__mocks__/react-native-i18n.js
+++ example/src/__mocks__/react-native-i18n.js
@@ -3,5 +3,5 @@
 import I18nJs from 'i18n-js';
 
 I18nJs.locale = 'en'; // a locale from your available translations
-export const getLanguages = () => Promise.resolve(['en']);
+export const getLanguages = (): Promise<string[]> => Promise.resolve(['en']);
 export default I18nJs;
